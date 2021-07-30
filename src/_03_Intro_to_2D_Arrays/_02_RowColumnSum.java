c766ab7149b78331f43de0b6b1c397421c770824
League Student <league@iMac-20.attlocal.net>
2021-07-29 18:34:09 -0700

    Title

--- src/_03_Intro_to_2D_Arrays/_02_RowColumnSum.java
+++ src/_03_Intro_to_2D_Arrays/_02_RowColumnSum.java
@@ -3,12 +3,25 @@ package _03_Intro_to_2D_Arrays;
 public class _02_RowColumnSum {
 
     public int getSumByRow(int[][] matrix, int row) {
+    	int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+			int a = matrix[row][i];
+			sum += a;
+		}
+        System.out.println("Row:" + sum);
         
-        return 0;
+        
+        return sum;
     }
     
     public int getSumByColumn(int[][] matrix, int col) {
-        
-        return 0;
+       	int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+			int a = matrix[i][col];
+			sum += a;
+
+		}
+        System.out.println("Column:" + sum);
+        return sum;
     }
 }
