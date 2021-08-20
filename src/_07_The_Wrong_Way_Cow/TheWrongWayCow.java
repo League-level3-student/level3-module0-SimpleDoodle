6bf9556453c6b45e809f9f0de4173a1ba3437b3d
League Student <league@iMac-34.attlocal.net>
2021-08-19 18:30:18 -0700

    Cowow

--- src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
+++ src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
@@ -51,6 +51,16 @@ package _07_The_Wrong_Way_Cow;
 public class TheWrongWayCow {
 
     public static int[] findWrongWayCow(final char[][] field) {
+    	for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field.length; j++) {
+				if (field[i][j] == 'c') {
+					int[] coord = new int[2];
+					coord[0] = i;
+					coord[1] = j;
+					return coord;
+				}
+			}
+		}
         // Fill in the code to return the [col, row] coordinate position of the
         // head (letter 'c') of the wrong way cow!
         
